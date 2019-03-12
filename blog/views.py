64a03be5b8485495e412f4ad42ec29e4ecb730f6
Yasmin <ym222bs@student.lnu.se>
2019-03-12 14:59:11 +0100

    routing ok

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home')
+    
+def about(request):
+    return HttpResponse('About')
