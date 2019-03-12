471dd1355cc75fe3bf00f75f9e85240d6c07dd52
Yasmin <ym222bs@student.lnu.se>
2019-03-12 20:54:43 +0100

    starting html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse('Home')
-    
+    return render(request, 'blog/home.html')
+
 def about(request):
     return HttpResponse('About')
+
